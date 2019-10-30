aa49c59561756d042d747fd2b018cd7c1a82785e
Harish Saravanakumar <hs3209@nyu.edu>
2019-10-30 17:35:58 -0400

    fixed bugs (logout, incorrect pwd, 2fa)

--- test_app.py
+++ test_app.py
@@ -1,16 +1,4 @@
 import requests, unittest
-from bs4 import BeautifulSoup
-import pytest
-import app
-import os
-from bs4 import BeautifulSoup
-WORDLIST = "wordlist.txt"
-
-# @pytest.fixture
-# def app():
-#     testing_app = app.run()
-#     testing_app.debug = True
-#     return testing_app.test_client()
 
 
 class TestSpellCheckApp(unittest.TestCase):
@@ -24,5 +12,5 @@ class TestSpellCheckApp(unittest.TestCase):
         assert (requests.get("http://localhost:5000/spell_check").status_code == 200)
 
 
-if __name__=='__main__':
-	unittest.main()
+if __name__ == '__main__':
+    unittest.main()
